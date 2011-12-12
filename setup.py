7dda9617a977c65985568403d89799ef4c1a265a
Jonas Obrist <me@ojii.ch>
2011-12-12 12:09:40 +0100

    let's support 1.2!

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires = [
         'django-load>=1.0.0',
         'django-classy-tags>=0.3.4.1',
-        'django>=1.3',
+        'django>=1.2',
     ],
     package_data={
         'mailchimp': [
